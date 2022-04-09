3389dc50036e32e89f777698a4d21a6918cad3a0
ebc@12345678 <sharadapatil1234@gmail.com>
2022-04-09 16:12:05 +0530

    hiiiiiii

---
+++ VENUE/src/b/NewTest1.java
@@ -0,0 +1,14 @@
+package b;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest1 {
+  @Test
+  public void c() {
+  }
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
